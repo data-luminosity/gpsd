bcb2bd4158d1dc1b2f396a0cc320764129eca473
josh <jjoy@cs.ucla.edu>
2016-06-30 08:16:57 -0700

    include app id in log filename

--- test_robust.sh
+++ test_robust.sh
@@ -5,18 +5,18 @@ test_num_clients ()
         #5 seconds
         if [ $((i%3)) -eq 0 ]
         then
-            ./cgps 10 0 > client_log/test_r$1_$i.txt &
+            ./cgps 10 0 > client_log/test_r$1_$i_0.txt &
         fi
 
         #10 seconds
         if [ $((i%3)) -eq 1 ] 
         then
-            ./cgps 10 50 > client_log/test_r$1_$i.txt &
+            ./cgps 10 50 > client_log/test_r$1_$i_50.txt &
         fi
         #15 seconds
         if [ $((i%3)) -eq 2 ]
         then
-            ./cgps 10 99 > client_log/test_r$1_$i.txt &
+            ./cgps 10 99 > client_log/test_r$1_$i_99.txt &
         fi
     done
 }
